c1f3c869e1c24f67226bda439e959ddb43f75042
longwei66 <longwei66@gmail.com>
2017-04-06 04:21:37 +0800

    update url of data sources

--- projects/aqi/getting-cleaning-data/scrape-weather-data_weatherbase.R
+++ projects/aqi/getting-cleaning-data/scrape-weather-data_weatherbase.R
@@ -1,68 +1,51 @@
 ## =============================================================================
-##
 ##      Scrape Weather hourly data from weatherbase
-##
 ##      For the cities : Shanghai, Beijing, Chengdu, Guangzhou, Shenyang, Paris
-##
 ## =============================================================================
 
 ## Needed libraries and system configuration
+## -----------------------------------------
 library(XML)
-library("rvest")
-library("dplyr")
-library("lubridate")
+library(rvest)
+library(dplyr)
+library(lubridate)
+library(data.table)
 Sys.setlocale(category = "LC_ALL", locale = "C")
 
 ## Data Configuration
-# City Url
-uriCityWeather <-
-        data.frame(
-                city = c(
-                        "Shanghai",
-                        "Beijing",
-                        "Chengdu",
-                        "Guangzhou",
-                        "Shenyang",
-                        "Paris"
-                ),
-                url = c(
-                        "http://www.weatherbase.com/weather/weatherhourly.php3?s=76385&cityname=Shanghai-Shanghai-China&date=",
-                        "http://www.weatherbase.com/weather/weatherhourly.php3?s=11545&date=2016-03-22&cityname=Beijing-Beijing-China&date=",
-                        "http://www.weatherbase.com/weather/weatherhourly.php3?s=49265&cityname=Chengdu-Sichuan-China&date=",
-                        "http://www.weatherbase.com/weather/weatherhourly.php3?s=78295&cityname=Guangzhou-Guangdong-China&date=",
-                        "http://www.weatherbase.com/weather/weatherhourly.php3?s=24345&cityname=Shenyang-Liaoning-China&date=",
-                        "http://www.weatherbase.com/weather/weatherhourly.php3?s=94170&cityname=Paris-Ile-de-France-France&date="
-                )
-        )
+## -----------------------------------------
 
+## Cities : city names and url to get weather
+uriCityWeather <- read.csv(file = "./data/share/aqi/weather-cities-data-source-url.csv", 
+                           sep = ";", 
+                           header = TRUE)
 
-
-
-# Plan
-nbDay <- 365
-
+## Configure scrapping plan, which cities, which year ?
 listOfCityYear <- data.frame(
         year = c(
-                #"2015","2015","2015","2015","2015","2015",
-                #"2014","2014","2014","2014","2014","2014",
-                #"2013",
-                "2013","2013","2013","2013","2013"),
-        city = c(
-                #"Shanghai","Beijing","Chengdu","Guangzhou","Shenyang","Paris",
-                #"Shanghai","Beijing","Chengdu","Guangzhou","Shenyang","Paris",
-                #"Shanghai",
-                "Beijing","Chengdu","Guangzhou","Shenyang","Paris"
-                 )
+                "2016","2016","2016","2016","2016","2016" #,
+                #"2015","2015","2015","2015","2015","2015" #,
+                #"2014","2014","2014","2014","2014","2014" #,
+                #"2013","2013","2013","2013","2013","2013"
+                ),
+        city = c(#
+                "Shanghai","Beijing","Chengdu","Guangzhou","Shenyang","Paris" #,
+                #"Shanghai","Beijing","Chengdu","Guangzhou","Shenyang","Paris" #,
+                #"Shanghai","Beijing", "Chengdu","Guangzhou","Shenyang","Paris"#,
+                #"Shanghai","Beijing","Chengdu","Guangzhou","Shenyang","Paris"
+        )
 )
 
+# How many days should we scrape in each year
+nbDay <- 365
 
 
 ## Loop per each year of the plant
 for (j in 1:nrow(listOfCityYear)) {
-
-        # Dataframe for storing city weather
-        weather <- data.frame(matrix(nrow = 1, ncol = 16))
-        names(weather) =  c(
+        
+        # Data.table for storing city weather
+        weather <- as.data.frame(matrix(nrow = 1, ncol = 16))
+        names(weather) <- c(
                 "City",
                 "date_obs",
                 "date.time",
@@ -78,11 +61,8 @@ for (j in 1:nrow(listOfCityYear)) {
                 "Precipitation",
                 "Events",
                 "Conditions",
-                "Wind.Direction.degree"
-        )
+                "Wind.Direction.degree")
         weather$date.time <- as.POSIXlt(weather$date.time)
-        
-        
         
         # Starting Date
         year <- listOfCityYear[j,]$year
@@ -90,117 +70,115 @@ for (j in 1:nrow(listOfCityYear)) {
         date <- strptime(day, "%Y-%m-%d")
         date_obs <- date
         
-        ## LOOP 365*2 days
+        ## LOOP 365 days
         for (i in 1:nbDay) {
                 ## Create data frame with
                 day <- as.character(date)
                 city <- listOfCityYear[j,]$city
                 year <- listOfCityYear[j,]$year
                 fileUrl <-paste(uriCityWeather[as.character(uriCityWeather$city) == city,]$url,day,"&units=metric",sep = "")
-                        
+                
                 # Xpath to get the whole Table
                 # //*[@id="left-weather-content"]/table[3]
-                        DayWeather <- fileUrl %>%
-                                read_html() %>%
-                                html_nodes(xpath = '//*[@id="left-weather-content"]/table[3]') %>%
-                                html_table()
-                        
-                        ## Continue if table is not empty
-                        if (length(DayWeather) > 0) {
-                                DayWeather <- as.data.frame(DayWeather)
-                                names(DayWeather) =  c(
-                                        "Local.Time",
-                                        "Temperature",
-                                        "Dewpoint",
-                                        "Humidity",
-                                        "Barometer",
-                                        "Visibility",
-                                        "Wind.Direction.code",
-                                        "Wind.Speed",
-                                        "Gust.Speed",
-                                        "Precipitation",
-                                        "Events",
-                                        "Conditions"
-                                )
-                                
-                                ## fill data for date and city
-                                DayWeather$date.time <- NA
-                                DayWeather$City <- city
-                                DayWeather$date_obs <- day
-                                
-                                DayWeather$date.time <-paste(DayWeather$date_obs,DayWeather$Local.Time)
-                                DayWeather$date.time <-strptime(DayWeather$date.time,format = "%Y-%m-%d %I:%M %p")
-                                
-                                
-                                ## Clean Data
-                                DayWeather$Temperature <- as.numeric(gsub(pattern = "(.*) \302\260C","\\1",DayWeather$Temperature))
-
-                                # Dewpoint in degree C
-                                DayWeather$Dewpoint <-as.numeric(gsub(pattern = "(.*) \302\260C","\\1",DayWeather$Dewpoint))
-
-                                                                # Humidity in %
-                                DayWeather$Humidity <- as.numeric(gsub(pattern = "(.*) %","\\1",DayWeather$Humidity))
-
-                                # Pressure in hPa
-                                DayWeather$Barometer <- as.numeric(gsub(pattern = "(.*) hPa","\\1",DayWeather$Barometer))
-
-                                # Visibility in km
-                                DayWeather$Visibility <-as.numeric(gsub(pattern = "(.*) km","\\1",DayWeather$Visibility))
-
-
-                                # Wind direction
-                                DayWeather$Wind.Direction.degree <-as.numeric(gsub(pattern = ".* \\((.*)\302\260\\)","\\1",DayWeather$Wind.Direction.code))
-                                DayWeather$Wind.Direction.code <- gsub(pattern = "(.*) \\(.*\260\\)","\\1", DayWeather$Wind.Direction.code)
-                                
-                                # Wind Speed
-                                DayWeather$Wind.Speed <- as.numeric( gsub(pattern = "(.*) km/h","\\1",DayWeather$Wind.Speed))
-                                
-                                # Gust speed
-                                DayWeather$Gust.Speed <- as.numeric( gsub(pattern = "(.*) km/h","\\1",DayWeather$Gust.Speed))
-                                
-                                ## Bind with the past records
-                                weather <- rbind(weather, DayWeather)
-                                
-                                # Success message
-                                message(paste(city,"/",date,":",nrow(DayWeather),
-                                                "records @", fileUrl))
-                        }
-                        else {
-                                # Error message
-                                message(paste(city,"/",date,"Failed ...    @",fileUrl))
-                        }
-                        # Bind with previous data records
-                        weather <- rbind(weather, DayWeather)
-                        # retract one day
-                        date <- date - 60 * 24 * 60
-                }
+                DayWeather <- fileUrl %>%
+                        read_html() %>%
+                        html_nodes(xpath = '//*[@id="left-weather-content"]/table[3]') %>%
+                        html_table()
                 
+                ## Continue if table is not empty
+                if (length(DayWeather) > 0) {
+                        DayWeather <- as.data.frame(DayWeather)
+                        names(DayWeather) =  c(
+                                "Local.Time",
+                                "Temperature",
+                                "Dewpoint",
+                                "Humidity",
+                                "Barometer",
+                                "Visibility",
+                                "Wind.Direction.code",
+                                "Wind.Speed",
+                                "Gust.Speed",
+                                "Precipitation",
+                                "Events",
+                                "Conditions"
+                        )
+                        
+                        ## fill data for date and city
+                        DayWeather$date.time <- NA
+                        DayWeather$City <- city
+                        DayWeather$date_obs <- day
+                        
+                        DayWeather$date.time <-paste(DayWeather$date_obs,DayWeather$Local.Time)
+                        DayWeather$date.time <-strptime(DayWeather$date.time,format = "%Y-%m-%d %I:%M %p")
+                        
+                        
+                        ## Clean Data
+                        DayWeather$Temperature <- as.numeric(gsub(pattern = "(.*) \302\260C","\\1",DayWeather$Temperature))
+                        
+                        # Dewpoint in degree C
+                        DayWeather$Dewpoint <-as.numeric(gsub(pattern = "(.*) \302\260C","\\1",DayWeather$Dewpoint))
+                        
+                        # Humidity in %
+                        DayWeather$Humidity <- as.numeric(gsub(pattern = "(.*) %","\\1",DayWeather$Humidity))
+                        
+                        # Pressure in hPa
+                        DayWeather$Barometer <- as.numeric(gsub(pattern = "(.*) hPa","\\1",DayWeather$Barometer))
+                        
+                        # Visibility in km
+                        DayWeather$Visibility <-as.numeric(gsub(pattern = "(.*) km","\\1",DayWeather$Visibility))
+                        
+                        
+                        # Wind direction
+                        DayWeather$Wind.Direction.degree <-as.numeric(gsub(pattern = ".* \\((.*)\302\260\\)","\\1",DayWeather$Wind.Direction.code))
+                        DayWeather$Wind.Direction.code <- gsub(pattern = "(.*) \\(.*\260\\)","\\1", DayWeather$Wind.Direction.code)
+                        
+                        # Wind Speed
+                        DayWeather$Wind.Speed <- as.numeric( gsub(pattern = "(.*) km/h","\\1",DayWeather$Wind.Speed))
+                        
+                        # Gust speed
+                        DayWeather$Gust.Speed <- as.numeric( gsub(pattern = "(.*) km/h","\\1",DayWeather$Gust.Speed))
+                        
+                        ## Bind with the past records
+                        weather <- rbind(weather, DayWeather)
+                        
+                        # Success message
+                        message(paste(city,"/",date,":",nrow(DayWeather),
+                                      "records @", fileUrl))
+                }
+                else {
+                        # Error message
+                        message(paste(city,"/",date,"Failed ...    @",fileUrl))
+                }
+                # Bind with previous data records
+                weather <- rbind(weather, DayWeather)
+                # retract one day
+                date <- date - 60 * 24 * 60
+        }
+        
         weather <- dplyr::select(weather,
-                                    City, # City Name
-                                    date.time,  # Date and time, local in PosixLt
-                                    date_obs, # Date in chr
-                                    Local.Time, # local time in AM/PM format
-                                    Temperature, # Temperature in degree C
-                                    Dewpoint, # Temperature in degree C
-                                    Humidity, # Humidity in %
-                                    Barometer, # Pressure in hPa
-                                    Visibility, # in km
-                                    Wind.Direction.code, # NSEW
-                                    Wind.Direction.degree, # in degrees
-                                    Wind.Speed, # in km/h
-                                    Gust.Speed, # in km/h
-                                    Precipitation, # ??? 0
-                                    Events, # chr textual
-                                    Conditions #chr text
+                                 City, # City Name
+                                 date.time,  # Date and time, local in PosixLt
+                                 date_obs, # Date in chr
+                                 Local.Time, # local time in AM/PM format
+                                 Temperature, # Temperature in degree C
+                                 Dewpoint, # Temperature in degree C
+                                 Humidity, # Humidity in %
+                                 Barometer, # Pressure in hPa
+                                 Visibility, # in km
+                                 Wind.Direction.code, # NSEW
+                                 Wind.Direction.degree, # in degrees
+                                 Wind.Speed, # in km/h
+                                 Gust.Speed, # in km/h
+                                 Precipitation, # ??? 0
+                                 Events, # chr textual
+                                 Conditions #chr text
         )
         
-        
-        
-                ## Write temporary result
-                write.csv(weather,
-                        file = paste(
-                                '../data/chinaPlethData/local/weather/weatherbase/',year, "-","weather-", city, '.csv',
-                                sep = ""))
+        ## Write temporary result
+        write.csv(weather,
+                  file = paste(
+                          '../data/chinaPlethData/local/weather/weatherbase/',year, "-","weather-", city, '.csv',
+                          sep = ""))
 }
 
 
